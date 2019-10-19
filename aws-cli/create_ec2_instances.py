cfd77ef1b6885f22a29ec1c34b2cad47dea37d37
Colin But <colin.but@outlook.com>
2019-10-19 11:35:53 +0100

    expanded python script with user-data

--- aws-cli/create_ec2_instances.py
+++ aws-cli/create_ec2_instances.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 ami = 'ami-00a1270ce1e007c27'
@@ -5,7 +6,21 @@ no_of_instances = 1
 instance_type = 't2.micro'
 keyPair = 'MyLondonKP'
 
-aws_create_instance_cli_command ='aws ec2 run-instances --image-id ' + ami + ' --count ' + str(no_of_instances) + ' --instance-type ' + instance_type + ' --key-name ' + keyPair
+# TODO take this in as an arg
+provisioning_script = 'install_nodejs.sh'
+
+aws_cli_command_args = {
+    'ami': ami,
+    'no_of_instances': str(no_of_instances),
+    'instance_type': instance_type,
+    'keyPair' : keyPair,
+    'provisioning_script': provisioning_script
+}
+
+aws_create_instance_cli_command ="""
+    aws ec2 run-instances --image-id {ami} --count {no_of_instances} \ 
+    --instance-type {instance_type} --key-name {keyPair} --user-data 'echo curl --silent --location https://rpm.nodesource.com/setup_10.x | bash - && yum install -y nodejs'
+    """.format(**aws_cli_command_args)
 
 print("Running aws cli command: ")
 print(aws_create_instance_cli_command)
---
+++ aws-cli/provisioning-scripts/install_nodejs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl --silent --location https://rpm.nodesource.com/setup_10.x | bash -
+yum install -y nodejs
