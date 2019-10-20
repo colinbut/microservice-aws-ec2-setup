432c43f05b034cf5fc879699525e685ea6225326
Colin But <colin.but@outlook.com>
2019-10-20 23:18:11 +0100

    fix problem with running provisioner because of missing a security group that allows SSH

--- infrastructure-as-code/terraform/ec2/main.tf
+++ infrastructure-as-code/terraform/ec2/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "ec2_instance" {
     instance_type               = "${var.instance_type}"
     key_name                    = "${var.key_name}"
     associate_public_ip_address = true
+    vpc_security_group_ids      = ["sg-0f89322a79aa4a404"] # ensure security group has SSH access
 
     tags = {
         Name = "Server"
