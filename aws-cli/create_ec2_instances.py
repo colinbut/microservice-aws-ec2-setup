5d5d4d70e4772f872bb6e3d6f761b51710a22f68
Colin But <colin.but@outlook.com>
2019-10-22 23:28:31 +0100

    completed python script to create ec2 instances accepting args

--- aws-cli/create_ec2_instances.py
+++ aws-cli/create_ec2_instances.py
@@ -1,13 +1,31 @@
 import os
 import subprocess
+import argparse
 
-ami = 'ami-00a1270ce1e007c27'
-no_of_instances = 1
-instance_type = 't2.micro'
-keyPair = 'MyLondonKP'
+JAVA = "java"
+NODE_JS = "nodejs"
 
-# TODO take this in as an arg
-provisioning_script = 'install_nodejs.sh'
+parser = argparse.ArgumentParser()
+parser.add_argument("project_type", help="the type of project to create ec2 instances for", choices=[JAVA, NODE_JS])
+parser.add_argument("--no-of-instances", help="number of ec2 instances to provision", type=int, default=1)
+parser.add_argument("--instance-type", help="the type of ec2 instance to provision", default="t2.micro")
+parser.add_argument("--ami", help="the ami id to provide", default="ami-00a1270ce1e007c27")
+parser.add_argument("--key-name", help="The name of the key pair to use for SSH access", default="MyLondonKP")
+parser.add_argument("--dry-run", help="Dry run?", action="store_true")
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+args = parser.parse_args()
+
+ami = args.ami
+no_of_instances = args.no_of_instances
+instance_type = args.instance_type
+keyPair = args.key_name
+
+project_type_provisioning_script_mapping = {
+    JAVA : "install_java.sh",
+    NODE_JS : "install_nodejs.sh"
+}
+
+provisioning_script = project_type_provisioning_script_mapping[args.project_type]
 
 aws_cli_command_args = {
     'ami': ami,
@@ -19,7 +37,12 @@ aws_cli_command_args = {
 
 aws_create_instance_cli_command = "aws ec2 run-instances --image-id {ami} --count {no_of_instances} --instance-type {instance_type} --key-name {keyPair} --user-data file://provisioning-scripts/{provisioning_script}".format(**aws_cli_command_args)
 
-print("Running aws cli command: ")
-print(aws_create_instance_cli_command)
+if args.verbose:
+    print("Running aws cli command: ")
+    print(aws_create_instance_cli_command)
 
-subprocess.call(aws_create_instance_cli_command.split(' '))
+if args.dry_run:
+    print("Executing script as a dry run - turn on verbose option using --verbose to see command being executed")
+else:
+    print("Executing...")
+    subprocess.call(aws_create_instance_cli_command.split(' '))
